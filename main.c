710f8176d602232f34a3a75e3740cb6692fcdce1
evanjohns <evanlloydjohns@gmail.com>
2020-09-24 17:57:31 -0400

    - Switched from using global variables for current and birth date to using local. - Added more functionality for error catching. - Fixed issues with isLeap() and dateValid()

--- main.c
+++ main.c
@@ -10,27 +10,23 @@
 #include <string.h>
 #include <time.h>
 
-#define AGE_MAX 122
+#define AGE_MAX 122 // Oldest person to ever live
 #define AGE_MIN -1
 
-void getData(void);
-int calcAge(int, int, int);
+void getData(int *, int *, int *);
+int calcAge(int, int, int, int, int, int);
 int concat(int, int, int);
 int calcMax(int);
 float calcUp(int);
 float calcLo(int);
-int dateValid(void);
+int dateValid(int, int, int, int);
 int isLeap(int);
 
-int day;    // User's birth day
-int month;  // User's birth month
-int year;   // User's birth year
-
-int currentDay;
-int currentMonth;
-int currentYear;
-
 int main(int argc, const char * argv[]) {
+
+    int bDay;    // User's birth day
+    int bMonth;  // User's birth month
+    int bYear;   // User's birth year
 
     int age;                // User's age
     int maxHeartRate;       // User's max heart rate
@@ -40,23 +36,16 @@ int main(int argc, const char * argv[]) {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
 
-    currentDay = tm.tm_mday;            // tm day is current day
-    currentMonth = tm.tm_mon + 1;		// tm_mon is months from 0-11
-    currentYear = tm.tm_year + 1900;	// tm_year is years since 1900
+    int cDay = tm.tm_mday;          // Current day
+    int cMonth = tm.tm_mon + 1;		// Current month
+    int cYear = tm.tm_year + 1900;	// Current year
 
-//    //TODO: Combine these statements into one loop
-//    do {
-//        getData();
-//    } while (!dateValid());
-//
-//    age = calcAge(day, month, year);
-//
-//    // Error checking user's age
-//    while (age > AGE_MAX || age < AGE_MIN) {
-//        puts("ERROR: Invalid birth date! Try again");
-//        getData();
-//        age = calcAge(day, month, year);
-//    }
+    do {
+        getData(&bDay, &bMonth, &bYear);
+        age = calcAge(bDay, bMonth, bYear, cDay, cMonth, cYear);
+    } while ((!dateValid(bDay, bMonth, bYear, cYear) ? puts("ERROR: That date isn't possible! try again") : printf("")) ||
+             (age > AGE_MAX ? puts("ERROR: No one can be that old! try again") : printf("")) ||
+             (age < AGE_MIN ? puts("ERROR: You haven't been born yet! try again") : printf("")));
 
     maxHeartRate = calcMax(age);
     upperHeartRate = calcUp(age);
@@ -67,6 +56,90 @@ int main(int argc, const char * argv[]) {
            "%.1fbpm - %.1fbpm\n", age, maxHeartRate, lowerHeartRate, upperHeartRate);
     puts("-------------------------------------------------\n");
 
+    return 0;
+}
+
+// Get input data from the user
+void getData(int * day, int * month, int * year) {
+    printf("\nEnter your birthday (MM/DD/YYYY): ");
+    if (scanf("%d/%d/%d", month, day, year) != 3) {
+        puts("ERROR: Invalid input type");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Calculates the difference between the concatenated birth date
+// and the concatenated current date resulting in the age of the user
+int calcAge(int bDay, int bMonth, int bYear, int cDay, int cMonth, int cYear) {
+    int bDate = concat(bDay, bMonth, bYear);
+    int cDate = concat(cDay, cMonth, cYear);
+
+    return (cDate - bDate) / 10000;
+}
+
+// concatenate the year, month, day into one integer
+int concat(int day, int month, int year) {
+    char string1[3];
+    char string2[3];
+    char string3[5];
+    char result[9];
+
+    sprintf(string1, "%02d", day);
+    sprintf(string2, "%02d", month);
+    sprintf(string3, "%d", year);
+
+    strcpy(result, string3);
+    strcat(result, string2);
+    strcat(result, string1);
+
+    return atoi(result);
+}
+
+// Checks if the date is valid
+int dateValid(int day, int month, int year, int cYear) {
+    if (year < 1800 || year > cYear) {
+        return 0;
+    }
+    if (month < 1 || month > 12) {
+        return 0;
+    }
+    if (day < 1 || day > 31) {
+        return 0;
+    }
+
+    // Check day is in month
+    if (month == 2) {
+        if (isLeap(year)) {
+            if (day > 29) {
+                return 0;
+            }
+        } else if (day > 28) {
+            return 0;
+        }
+    }
+
+    // Check months with 30 days
+    if (month == 4 || month == 6 || month == 9 || month == 11) {
+        if (day <= 30) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Checks if the year is a leap year
+int isLeap(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return 1;
+            }
+        } else {
+            return 1;
+        }
+    }
     return 0;
 }
 
@@ -83,83 +156,4 @@ float calcUp(int age) {
 // Calculate the user's lower target heart rate
 float calcLo(int age) {
     return (float) (0.5 * calcMax(age));
-}
-
-// Get input data from the user
-void getData() {
-    printf("Enter your birthday (MM/DD/YYYY): ");
-     if (scanf("%d/%d/%d", &month, &day, &year) != 3) {
-         puts("ERROR: Invalid input");
-         exit(EXIT_FAILURE);
-     }
-}
-
-// concatenate the year, month, day into one integer
-int concat(int d, int m, int y) {
-    char string1[3];
-    char string2[3];
-    char string3[5];
-    char result[9];
-
-    sprintf(string1, "%02d", d);
-    sprintf(string2, "%02d", m);
-    sprintf(string3, "%d", y);
-
-    strcpy(result, string3);
-    strcat(result, string2);
-    strcat(result, string1);
-
-    return atoi(result);
-}
-
-// Calculates the difference between the concatenated birth date
-// and the concatenated current date resulting in the age of the user
-int calcAge(int birthDay, int birthMonth, int birthYear) {
-    int birthDate = concat(birthDay, birthMonth, birthYear);
-    int currentDate = concat(currentDay, currentMonth, currentYear);
-
-    return (currentDate - birthDate) / 10000;
-}
-
-// Checks if the year is a leap year
-int isLeap(int y) {
-    if((y % 4 == 0) && (y % 100 != 0) && (y % 400 == 0)) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-// Checks if the date is valid
-int dateValid() {
-    if (year < 1800 || year > currentYear) {
-        return 0;
-    }
-    if (month < 1 || month > 12) {
-        return 0;
-    }
-    if (day < 1 || day > 31) {
-        return 0;
-    }
-
-    // Check day is in month
-    if (month == 2) {
-        if (isLeap(year)) {
-            if (day <= 29) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-
-    // Check months with 30 days
-    if (month == 4 || month == 6 || month == 9 || month == 11) {
-        if (day <= 30) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-    return 1;
 }
