7f7e4b5170f87e7af01bd850a21a59d70cb4240c
evanjohns <evanlloydjohns@gmail.com>
2020-09-24 09:48:01 -0400

    First push

---
+++ cmake-build-debug/CMakeFiles/TargetHeartRate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TargetHeartRate.dir/main.c.o"
+  "TargetHeartRate"
+  "TargetHeartRate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TargetHeartRate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
